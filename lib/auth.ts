fe251036fc80bef5f05707db303922a7d0e9fc58
Dhruva Agrawal <dhruva1998@gmail.com>
2023-07-06 16:13:01 +0530

    Added basic implementation for next-auth + added enum for HTTP status codes

---
+++ lib/auth.ts
@@ -0,0 +1,14 @@
+import { NextAuthOptions, getServerSession } from "next-auth"
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+    // NextAuth sets a default `maxAge` of 30 days (which is 60 * 60 * 24 * 30), but that is a very large
+    // value, and hence, for now, setting the `maxAge` to 2 minutes (i.e. 60 * 60).
+    maxAge: 2 * 60,
+  },
+  providers: [],
+  callbacks: {},
+}
+
+export const getAuthSession = () => getServerSession(authOptions)
